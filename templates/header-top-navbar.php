8b9d67a79bfa38188b5aeffde8dcf6c979b0db3b
Mason L <mason.lawlor@me.com>
2014-08-29 20:57:23 -0700

    Revert 6d8a3f7..eb41d12
    This rolls back to commit 6d8a3f737b7c058dd15a9df6df2fdce727e33d01.

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,20 +1,35 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
     <div class="navbar-header">
-	    <div class="nav-icon">
-	    <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img class="" src="http://masonlawlor.com//assets/icon.png" alt="logo-nav" width="auto" height="32px" /></a>
-	    </div>
-	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-	        <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	      </button>
+      <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img class="" src="http://masonlawlor.com//assets/icon.png" alt="logo-nav" width="auto" height="32px" /></a>
     </div>
-
-    <nav class="collapse navbar-collapse" role="navigation">
-    <button href="#" type="button" class="pull-right btn btn-default hero-btn login-btn" data-toggle="modal" data-target="#myModal">Login</button>
-      
+    <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+      <!--<ul class="nav navbar-nav">
+        <li>
+          <a href="../getting-started">Getting started</a>
+        </li>
+        <li class="active">
+          <a href="../css">CSS</a>
+        </li>
+        <li>
+          <a href="../components">Components</a>
+        </li>
+        <li>
+          <a href="../javascript">JavaScript</a>
+        </li>
+        <li>
+          <a href="../customize">Customize</a>
+        </li>
+      </ul>-->
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#" type="button" class="" data-toggle="modal" data-target="#myModal">Login</a</li>
+      </ul>
     </nav>
   </div>
 </header>
