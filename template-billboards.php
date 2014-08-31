bd28a187b5b22721db0db32aa361dbef80461c37
Mason L <mason.lawlor@me.com>
2014-08-31 03:07:23 -0700

    added content to billboards page

--- template-billboards.php
+++ template-billboards.php
@@ -6,3 +6,52 @@ Template Name: Billboard Template
 
 <?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page'); ?>
+
+<body>
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img data-src="holder.js/300x300" alt="...">
+						<div class="caption">
+							<h3>Billboard 1</h3>
+							<p>Lorem Ipsum Dalorious Maximus YEah!</p>
+							<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img data-src="holder.js/300x300" alt="...">
+						<div class="caption">
+							<h3>Billboard 2</h3>
+							<p>Lorem Ipsum Dalorious Maximus YEah!</p>
+							<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img data-src="holder.js/300x300" alt="...">
+						<div class="caption">
+							<h3>Billboard 3</h3>
+							<p>Lorem Ipsum Dalorious Maximus YEah!</p>
+							<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img data-src="holder.js/300x300" alt="...">
+						<div class="caption">
+							<h3>Billboard 4</h3>
+							<p>Lorem Ipsum Dalorious Maximus YEah!</p>
+							<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</body>
