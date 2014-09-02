0bc29317f2d1eff3625269f63081d4496591dc4f
Mason L <mason.lawlor@me.com>
2014-09-01 17:38:03 -0700

    billboard page layout

--- template-billboards.php
+++ template-billboards.php
@@ -9,6 +9,19 @@ Template Name: Billboard Template
 
 <body>
 	<section>
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="google-maps">
+					<iframe src="https://mapsengine.google.com/map/embed?mid=zyIDL-CK3Aww.k3PzmkiDW3WY" width="640" height="480"></iframe>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<h3>View Bigger Map</h3>
+			</div>
+		</div>
+	</section>
+
+	<section>
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-6 col-md-3">
@@ -16,8 +29,8 @@ Template Name: Billboard Template
 						<img data-src="holder.js/300x300" alt="...">
 						<div class="caption">
 							<h3>Billboard 1</h3>
-							<p>Lorem Ipsum Dalorious Maximus YEah!</p>
-							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-1/" class="btn btn-primary" role="button">Preview</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><strong>Outbound:</strong> Edif. Baldorioty Billboard</p>
+							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-1/" class="btn btn-primary" role="button">Select</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -26,8 +39,8 @@ Template Name: Billboard Template
 						<img data-src="holder.js/300x300" alt="...">
 						<div class="caption">
 							<h3>Billboard 2</h3>
-							<p>Lorem Ipsum Dalorious Maximus YEah!</p>
-							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-2/" class="btn btn-primary" role="button">Preview</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><strong>Outbound:</strong> Outboard Billboard</p>
+							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-2/" class="btn btn-primary" role="button">Select</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -36,8 +49,8 @@ Template Name: Billboard Template
 						<img data-src="holder.js/300x300" alt="...">
 						<div class="caption">
 							<h3>Billboard 3</h3>
-							<p>Lorem Ipsum Dalorious Maximus YEah!</p>
-							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-3/" class="btn btn-primary" role="button">Preview</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><strong>Inbound:</strong> Pavia Billboard</p>
+							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-3/" class="btn btn-primary" role="button">Select</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -46,12 +59,102 @@ Template Name: Billboard Template
 						<img data-src="holder.js/300x300" alt="...">
 						<div class="caption">
 							<h3>Billboard 4</h3>
-							<p>Lorem Ipsum Dalorious Maximus YEah!</p>
-							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-4/" class="btn btn-primary" role="button">Preview</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p>Expreso de Diego (Cerca a Plaza)</p>
+							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-4/" class="btn btn-primary" role="button">Select</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
 			</div>
+			<div class="row">
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img data-src="holder.js/300x300" alt="...">
+						<div class="caption">
+							<h3>Billboard 5</h3>
+							<p><strong>Inbound:</strong> La Roca Billboard</p>
+							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-1/" class="btn btn-primary" role="button">Select</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img data-src="holder.js/300x300" alt="...">
+						<div class="caption">
+							<h3>Billboard 6</h3>
+							<p><strong>Inbound:</strong> Ochoa Billboard</p>
+							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-2/" class="btn btn-primary" role="button">Select</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img data-src="holder.js/300x300" alt="...">
+						<div class="caption">
+							<h3>Billboard 7</h3>
+							<p><strong>Inbound:</strong> Martinez Nadal - Entrando a Guaynabo</p>
+							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-3/" class="btn btn-primary" role="button">Select</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img data-src="holder.js/300x300" alt="...">
+						<div class="caption">
+							<h3>Billboard 8</h3>
+							<p><strong>Outbound:</strong> Martinez Nadal - Saliendo de Guaynabo</p>
+							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-4/" class="btn btn-primary" role="button">Select</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img data-src="holder.js/300x300" alt="...">
+						<div class="caption">
+							<h3>Billboard 9</h3>
+							<p><strong>Inbound:</strong> Expreso Las Americas (Por Plaza - Domenech)</p>
+							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-1/" class="btn btn-primary" role="button">Select</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img data-src="holder.js/300x300" alt="...">
+						<div class="caption">
+							<h3>Billboard 10</h3>
+							<p><strong>Outbound:</strong> Mayaguez</p>
+							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-2/" class="btn btn-primary" role="button">Select</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img data-src="holder.js/300x300" alt="...">
+						<div class="caption">
+							<h3>Billboard 11</h3>
+							<p><strong>Outbound:</strong> Dorado</p>
+							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-3/" class="btn btn-primary" role="button">Select</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="thumbnail">
+						<img data-src="holder.js/300x300" alt="...">
+						<div class="caption">
+							<h3>Billboard 12</h3>
+							<p>Expreso de Diego - Rexach</p>
+							<p><a href="http://masonlawlor.com/classificadosoutdoor/billboard-4/" class="btn btn-primary" role="button">Select</a> <a href="#" class="btn btn-default" role="button">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<section>
+		<div class="google-maps">
+			<iframe src="https://mapsengine.google.com/map/embed?mid=zyIDL-CK3Aww.k3PzmkiDW3WY" width="640" height="480"></iframe>
 		</div>
 	</section>
 </body>
