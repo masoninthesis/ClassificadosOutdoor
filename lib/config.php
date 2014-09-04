7c5ef41190ac8ccfa9dc3b7c38873466eda4ee5e
Mason L <mason.lawlor@me.com>
2014-09-04 02:00:17 -0700

    CSS to form changes. got basic version working

--- lib/config.php
+++ lib/config.php
@@ -56,7 +56,8 @@ function roots_display_sidebar() {
     array(
       'is_404',
       'is_front_page', 
-	  	array('is_page', array(2))    
+	  	array('is_page', array(2)),
+	  'is_single'  
 	),
     /**
      * Page template checks (via is_page_template())
