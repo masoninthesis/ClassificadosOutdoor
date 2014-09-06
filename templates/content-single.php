9f5baa77c1f6aadfa303f4b7f2f7c8d4aa926548
Mason L <mason.lawlor@me.com>
2014-09-05 19:23:38 -0700

    jesus fucking christ, i got it to work

--- templates/content-single.php
+++ templates/content-single.php
@@ -5,7 +5,17 @@
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-content">
+    	<div class="container">
+			<a href="#"><h3>Redo Billboard</h3></a>
+			<input type="button" id="btnSave" value="Save PNG">
+		</div>
+
+		<span id="widget" class="widget" field="AGE" roundby="20" description="Patient age, in years">
       <?php the_content(); ?>
+      </span>
+	  <br>
+
+	  <div id="img-out"><canvas width="362" height="30"></canvas></div>
     </div>
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
