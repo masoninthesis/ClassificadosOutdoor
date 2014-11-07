c36b6014792d16983a17c318b8cc9f648525c7a3
Mason L <mason.lawlor@me.com>
2014-11-06 20:37:27 -0800

    opaque nav, quantity and redirect to paypal, ....
    tooltip shortened

--- template-billboards.php
+++ template-billboards.php
@@ -50,7 +50,7 @@ Template Name: Billboard Template
 						<div class="caption">
 							<h3><a href="http://clasificadosoutdoor.com/billboard-1/">Billboard 1</a></h3>
 							<p><strong>Edif. Baldorioty Billboard </strong>(Outbound)</p>
-							<p><a href="http://clasificadosoutdoor.com/billboard-1/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip1" data-toggle="tooltip" data-placement="top" title="Call: (787)641-1191 or Email: info@clasificadosoutdoor.com">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><a href="http://clasificadosoutdoor.com/billboard-1/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip1" data-toggle="tooltip" data-placement="top" title="Llama 787-641-1191 o envía e-mail">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -60,7 +60,7 @@ Template Name: Billboard Template
 						<div class="caption">
 							<h3><a href="http://clasificadosoutdoor.com/billboard-2/">Billboard 2</a></h3>
 							<p><strong>Outboard Billboard </strong>(Outbound)</p>
-							<p><a href="http://clasificadosoutdoor.com/billboard-2/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip2" data-toggle="tooltip" data-placement="top" title="Call: (787)641-1191 or Email: info@clasificadosoutdoor.com">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><a href="http://clasificadosoutdoor.com/billboard-2/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip2" data-toggle="tooltip" data-placement="top" title="Llama 787-641-1191 o envía e-mail">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -70,7 +70,7 @@ Template Name: Billboard Template
 						<div class="caption">
 							<h3><a href="http://clasificadosoutdoor.com/billboard-3/">Billboard 3</a></h3>
 							<p><strong>Pavia Billboard </strong>(Inbound)</p>
-							<p><a href="http://clasificadosoutdoor.com/billboard-3/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip3" data-toggle="tooltip" data-placement="top" title="Call: (787)641-1191 or Email: info@clasificadosoutdoor.com">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><a href="http://clasificadosoutdoor.com/billboard-3/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip3" data-toggle="tooltip" data-placement="top" title="Llama 787-641-1191 o envía e-mail">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -80,7 +80,7 @@ Template Name: Billboard Template
 						<div class="caption">
 							<h3><a href="http://clasificadosoutdoor.com/billboard-4/">Billboard 4</a></h3>
 							<p><strong>Expreso de Diego</strong> (Cerca a Plaza)</p>
-							<p><a href="http://clasificadosoutdoor.com/billboard-4/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip4" data-toggle="tooltip" data-placement="top" title="Call: (787)641-1191 or Email: info@clasificadosoutdoor.com">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><a href="http://clasificadosoutdoor.com/billboard-4/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip4" data-toggle="tooltip" data-placement="top" title="Llama 787-641-1191 o envía e-mail">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -92,7 +92,7 @@ Template Name: Billboard Template
 						<div class="caption">
 							<h3><a href="http://clasificadosoutdoor.com/billboard-5/">Billboard 5</a></h3>
 							<p><strong>La Roca Billboard</strong>(Inbound)</p>
-							<p><a href="http://clasificadosoutdoor.com/billboard-5/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip5" data-toggle="tooltip" data-placement="top" title="Call: (787)641-1191 or Email: info@clasificadosoutdoor.com">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><a href="http://clasificadosoutdoor.com/billboard-5/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip5" data-toggle="tooltip" data-placement="top" title="Llama 787-641-1191 o envía e-mail">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -102,7 +102,7 @@ Template Name: Billboard Template
 						<div class="caption">
 							<h3><a href="http://clasificadosoutdoor.com/billboard-6/">Billboard 6</a></h3>
 							<p><strong>Ochoa Billboard </strong>(Inbound)</p>
-							<p><a href="http://clasificadosoutdoor.com/billboard-6/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip6" data-toggle="tooltip" data-placement="top" title="Call: (787)641-1191 or Email: info@clasificadosoutdoor.com">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><a href="http://clasificadosoutdoor.com/billboard-6/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip6" data-toggle="tooltip" data-placement="top" title="Llama 787-641-1191 o envía e-mail">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -112,7 +112,7 @@ Template Name: Billboard Template
 						<div class="caption">
 							<h3><a href="http://clasificadosoutdoor.com/billboard-7/">Billboard 7</a></h3>
 							<p><strong>Martinez Nadal - Entrando a Guaynabo </strong>(Inbound)</p>
-							<p><a href="http://clasificadosoutdoor.com/billboard-7/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip7" data-toggle="tooltip" data-placement="top" title="Call: (787)641-1191 or Email: info@clasificadosoutdoor.com">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><a href="http://clasificadosoutdoor.com/billboard-7/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip7" data-toggle="tooltip" data-placement="top" title="Llama 787-641-1191 o envía e-mail">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -122,7 +122,7 @@ Template Name: Billboard Template
 						<div class="caption">
 							<h3><a href="http://clasificadosoutdoor.com/billboard-8/">Billboard 8</a></h3>
 							<p><strong>Martinez Nadal - Saliendo de Guaynabo </strong>(Outbound)</p>
-							<p><a href="http://clasificadosoutdoor.com/billboard-8/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip8" data-toggle="tooltip" data-placement="top" title="Call: (787)641-1191 or Email: info@clasificadosoutdoor.com">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><a href="http://clasificadosoutdoor.com/billboard-8/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip8" data-toggle="tooltip" data-placement="top" title="Llama 787-641-1191 o envía e-mail">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -134,7 +134,7 @@ Template Name: Billboard Template
 						<div class="caption">
 							<h3><a href="http://clasificadosoutdoor.com/billboard-9/">Billboard 9</a></h3>
 							<p><strong>Expreso Las Americas (Por Plaza - Domenech) </strong>(Inbound)</p>
-							<p><a href="http://clasificadosoutdoor.com/billboard-9/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip9" data-toggle="tooltip" data-placement="top" title="Call: (787)641-1191 or Email: info@clasificadosoutdoor.com">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><a href="http://clasificadosoutdoor.com/billboard-9/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip9" data-toggle="tooltip" data-placement="top" title="Llama 787-641-1191 o envía e-mail">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -144,7 +144,7 @@ Template Name: Billboard Template
 						<div class="caption">
 							<h3><a href="http://clasificadosoutdoor.com/billboard-10/">Billboard 10</a></h3>
 							<p><strong>Mayaguez </strong>(Outbound)</p>
-							<p><a href="http://clasificadosoutdoor.com/billboard-10/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip10" data-toggle="tooltip" data-placement="top" title="Call: (787)641-1191 or Email: info@clasificadosoutdoor.com">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><a href="http://clasificadosoutdoor.com/billboard-10/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip10" data-toggle="tooltip" data-placement="top" title="Llama 787-641-1191 o envía e-mail">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -154,7 +154,7 @@ Template Name: Billboard Template
 						<div class="caption">
 							<h3><a href="http://clasificadosoutdoor.com/billboard-11/">Billboard 11</a></h3>
 							<p><strong>Dorado</strong> (Outbound)</p>
-							<p><a href="http://clasificadosoutdoor.com/billboard-11/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip11" data-toggle="tooltip" data-placement="top" title="Call: (787)641-1191 or Email: info@clasificadosoutdoor.com">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><a href="http://clasificadosoutdoor.com/billboard-11/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip11" data-toggle="tooltip" data-placement="top" title="Llama 787-641-1191 o envía e-mail">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
@@ -164,14 +164,14 @@ Template Name: Billboard Template
 						<div class="caption">
 							<h3><a href="http://clasificadosoutdoor.com/billboard-12/">Billboard 12</a></h3>
 							<p><strong>Expreso de Diego </strong>- Rexach</p>
-							<p><a href="http://clasificadosoutdoor.com/billboard-12/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip12" data-toggle="tooltip" data-placement="top" title="Call: (787)641-1191 or Email: info@clasificadosoutdoor.com">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
+							<p><a href="http://clasificadosoutdoor.com/billboard-12/" class="btn btn-primary" role="button">Seleccione</a> <a class="btn btn-default" role="button" id="myToolTip12" data-toggle="tooltip" data-placement="top" title="Llama 787-641-1191 o envía e-mail">Info <i class="glyphicon glyphicon-info-sign"></i></a></p>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</section>
-	
+
 	<section>
 		<div class="google-maps" id="maps">
 			<iframe src="https://mapsengine.google.com/map/embed?mid=zyIDL-CK3Aww.k3PzmkiDW3WY" width="640" height="480"></iframe>
--- templates/content-single.php
+++ templates/content-single.php
@@ -8,32 +8,17 @@
     	<div class="container">
         <div id="paypal" class="col-xs-12">
           <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-            <input type="hidden" name="cmd" value="_xclick">
-            <input type="hidden" name="business" value="info@clasificadosoutdoor.com">
-            <input type="hidden" name="lc" value="US">
-            <input type="hidden" name="item_name" value="Clasificados">
-            <input type="hidden" name="button_subtype" value="services">
-            <input type="hidden" name="no_note" value="0">
-            <input type="hidden" name="currency_code" value="USD">
-            <input type="hidden" name="bn" value="PP-BuyNowBF:paypal-btn.png:NonHostedGuest">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="68ZZS3TGNE2Y8">
             <table>
             <tr><td><input type="hidden" name="on0" value="Rates">Rates</td></tr><tr><td><select name="os0">
-              <option value="1440">1440 $150.00 USD</option>
-              <option value="720">720 $85.00 USD</option>
-              <option value="360">360 $45.00 USD</option>
-              <option value="180">180 $25.00 USD</option>
+            	<option value="1440">1440 $150.00 USD</option>
+            	<option value="720">720 $85.00 USD</option>
+            	<option value="360">360 $45.00 USD</option>
+            	<option value="180">180 $25.00 USD</option>
             </select> </td></tr>
             </table>
             <input type="hidden" name="currency_code" value="USD">
-            <input type="hidden" name="option_select0" value="1440">
-            <input type="hidden" name="option_amount0" value="150.00">
-            <input type="hidden" name="option_select1" value="720">
-            <input type="hidden" name="option_amount1" value="85.00">
-            <input type="hidden" name="option_select2" value="360">
-            <input type="hidden" name="option_amount2" value="45.00">
-            <input type="hidden" name="option_select3" value="180">
-            <input type="hidden" name="option_amount3" value="25.00">
-            <input type="hidden" name="option_index" value="0">
             <input class="pay-btn" type="image" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/paypal-btn.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
             <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
           </form>
