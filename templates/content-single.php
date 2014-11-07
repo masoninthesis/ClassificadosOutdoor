74b16adabdfc285081b8e7668852f2046c6d5238
Mason L <mason.lawlor@me.com>
2014-11-06 17:22:44 -0800

    last updates to paypal, still need quantity

--- templates/content-single.php
+++ templates/content-single.php
@@ -6,37 +6,56 @@
     </header>
     <div class="entry-content">
     	<div class="container">
-    		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-				<input type="hidden" name="cmd" value="_s-xclick">
-				<input type="hidden" name="hosted_button_id" value="LT4XPGCL9UHDU">
-				<table>
-					<tr><td><input type="hidden" name="on0" value="Seleccione Paquete">Seleccione Paquete</td></tr><tr><td><select name="os0">
-						<option value="1440">1440 $0.01 USD</option>
-						<option value="720">720 $0.01 USD</option>
-						<option value="360">360 $0.01 USD</option>
-						<option value="180">180 $0.01 USD</option>
-					</select> </td></tr>
-				</table>
-				<br>
-				<input type="hidden" name="currency_code" value="USD">
-				<input type="image" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/paypal-btn.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-			</form>
-
-			<a href="" class="btn btn-primary btn-pad" id="btnSave">
-				<i class="fa fa-cloud-download"> Save</i>
-			</a>
-			<a href="" class="btn btn-primary btn-pad" id="btnSave">
-				<i class="fa fa-undo"> Redo</i>
-			</a>
-
-			<span id="widget" class="widget">
-    			<?php the_content(); ?>
-    		</span>
-	  		<br>
-    		<div id="img-out"><canvas width="362" height="30"></canvas></div>
+        <div id="paypal" class="col-xs-12">
+          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_xclick">
+            <input type="hidden" name="business" value="info@clasificadosoutdoor.com">
+            <input type="hidden" name="lc" value="US">
+            <input type="hidden" name="item_name" value="Clasificados">
+            <input type="hidden" name="button_subtype" value="services">
+            <input type="hidden" name="no_note" value="0">
+            <input type="hidden" name="currency_code" value="USD">
+            <input type="hidden" name="bn" value="PP-BuyNowBF:paypal-btn.png:NonHostedGuest">
+            <table>
+            <tr><td><input type="hidden" name="on0" value="Rates">Rates</td></tr><tr><td><select name="os0">
+              <option value="1440">1440 $150.00 USD</option>
+              <option value="720">720 $85.00 USD</option>
+              <option value="360">360 $45.00 USD</option>
+              <option value="180">180 $25.00 USD</option>
+            </select> </td></tr>
+            </table>
+            <input type="hidden" name="currency_code" value="USD">
+            <input type="hidden" name="option_select0" value="1440">
+            <input type="hidden" name="option_amount0" value="150.00">
+            <input type="hidden" name="option_select1" value="720">
+            <input type="hidden" name="option_amount1" value="85.00">
+            <input type="hidden" name="option_select2" value="360">
+            <input type="hidden" name="option_amount2" value="45.00">
+            <input type="hidden" name="option_select3" value="180">
+            <input type="hidden" name="option_amount3" value="25.00">
+            <input type="hidden" name="option_index" value="0">
+            <input class="pay-btn" type="image" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/paypal-btn.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+          </form>
+        </div>
+        <div id="note" class="col-xs-5">
+          <div class="alert alert-warning">ó llamar 787-641-1191 para alternativa de pago</div>
+        </div>
+        <div id="btns" class="col-xs-12">
+          <a href="" class="btn btn-primary btn-pad" id="btnSave">
+            <i class="fa fa-cloud-download"> Save</i>
+          </a>
+          <a href="http://clasificadosoutdoor.com/escoja-su-billboard/" class="btn btn-primary btn-pad" id="btnSave">
+            <i class="fa fa-undo"> Redo</i>
+          </a>
+        </div>
+  			<span id="widget" class="widget">
+      			<?php the_content(); ?>
+      		</span>
+  	  		<br>
+      		<div id="img-out"><canvas width="362" height="30"></canvas></div>
+      	</div>
     	</div>
-  	</div>
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
     </footer>
